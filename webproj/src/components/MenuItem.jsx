1835616e7337b1adddbdf494619026ec0f6d4e6d
MpHuKTo <mphukto1337@yandex.ru>
2024-12-10 19:54:38 +0500

    упростил логику массива и добавил счетчик
    счетчик для продуктов, можно тыкать плюс и минус

--- webproj/src/components/MenuItem.jsx
+++ webproj/src/components/MenuItem.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './MenuItem.css'
 
+
+
 const MenuItem = (props) => {
+  const [productCount,SetPCount] = useState(0)
+  
+  const decreaseCount = () => {if(productCount > 0){SetPCount(productCount - 1)}};
+  const increaseCount = () => SetPCount(productCount + 1);
+
   return (
     <div className='MIbackground'>
       <img className='img' src={props.img}></img>
@@ -11,6 +18,12 @@ const MenuItem = (props) => {
         <div className='productDescription'>{props.description}</div>
 
         <div className='priceandweight'>{props.price} Тг.   за   {props.weight}</div>
+
+        <div className='buttonsContainer'>
+        <button className='minusOne' onClick={decreaseCount}><img src="/Img/Misc/minus.webp" alt="" className='minplusImages'/></button>
+        <p>{productCount}</p>
+        <button className='plusOne' onClick={increaseCount}><img src="/Img/Misc/plus.webp" alt="" className='minplusImages'/></button>
+        </div>
     </div>
   )
 }
